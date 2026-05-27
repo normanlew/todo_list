d7787aefc030af8ede9ab1b3836288e5dd0a37a0
Norman Lew <normanlew@users.noreply.github.com>
2026-05-27 16:55:34 -0700

    Add dates using Date function

--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ import { Project } from "./project.js";
 
 // console.log(test_message);
 
-const callCarRepair = new Todo("Call car repair", "Call dealership to do oil change", "January 10, 2027", "Low",
-    `This is the half-yearly service`, false);
+const callCarRepair = new Todo("Call car repair", "Call dealership to do oil change", new Date(2018, 11, 24, 10, 33, 30),
+    "Low", `This is the half-yearly service`, false);
 
 console.log(callCarRepair);
 
 console.log(callCarRepair.description);
 
 const lunchWithBob = new Todo("Schedule lunch with Bob", "Call Bob and the restaurant to set the time", 
-    "January 10, 2027", "Medium", "Bob will be in town for business", true);
+    new Date(2018, 1, 24, 17, 53, 45), "Medium", "Bob will be in town for business", true);
 
 
 const afternoonTodos = new Project("Afternoon");
